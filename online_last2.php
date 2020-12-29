c52e4b88831c7ae9c110a9e53b06da93d2c67482
jhengivy <yozuca1123@gmail.com>
2020-12-29 13:10:04 +0800

    motifly

---
+++ online_last2.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php require_once('i_meta.php'); ?>
+		<title>線上諮詢</title>
+	</head>
+	<body >
+		<?php require_once('i_header.php'); ?>
+		<section>
+			<div class="page_banner_pic" style="background-image:url(images/banner_02.png)" ;="">
+				<div class="page_title">
+					<div class="banner_title">線上諮詢</div>
+					<div class="page_p">輕鬆提問，讓小C幫您解決問題</div>
+				</div>
+			</div>
+			<div class="container all_wrapptop">
+				<div class="bread_crumb">
+					<ul>
+						<li><a href="index">首頁</a></li>
+						<li>/</li>
+						<li class="bread_active">線上諮詢</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<div class="info_title">一、市場商情</div>
+				<div class="line"></div>
+				<div class="row">
+					<div class="col-12 col-md-12 col-lg-12">
+						<div class="online_name">1.	海外貿易資訊</div>
+							<div class="onlineend_p">
+								電腦版：貿協全球資訊網 (<a href="https://www.taitraesource.com/" target="_blank">https://www.taitraesource.com/</a> )<br>
+								手機版：《全球經貿e 指通》App<br>
+							</div>
+							<ul class="number_li">
+								<li>蒐集全球171 個國家地區最新的經貿動態，產業現況及拓銷建議，讓業者可以隨時隨地查閱，時時刻刻掌握市場</li>
+								<li>針對所欲查詢之特定市場商情，在首頁設定「洲別」及「國家」後，按Return</li>
+							</ul>
+						<div class="online_name">2.	「台灣經貿網」商情電子報</div>
+
+						<div class="onlineend_p">
+							<div class="link"><a href="https://info.taiwantrade.com/biznews" target="_blank"><img src="images/link.svg" alt=""></a></div>
+							<div class="link_font"><a href="https://info.taiwantrade.com/biznews" target="_blank" >(https://info.taiwantrade.com/biznews)</a></div>
+						蒐集經濟部及外貿協會派駐全球100多國經貿人員所撰寫的最新海外市場即時商情。</div>
+
+						<ol class="number_li">
+							<li>在新興產業及全球熱點部分，商情主要包含下列主題：
+
+							</li>
+							<ol class="numberpoint_ul">
+							  <li>由貿協曾派駐海外、具海外市場推廣經驗的顧問提供諮詢。</li>
+							  <li>遴聘外部專家擔任客座諮詢顧問，提供全方位的國際行銷諮詢服務。</li>
+							  <li>提供海外市場拓銷建議，節省資源與時間的耗費。</li>
+							  <li>引導業者善用政府輔導資源，強化企業經營體質，提升國際競爭力。</li>
+							</ol>
+							<li>另有涵括生技醫療、機械、數位、車輛、美妝、資通訊等『焦點產業』單元</li>
+							<li>在地區商情部分，有涵括全球六大洲的『全球經貿商情』單元</li>
+							<li>除前述分類之全球產業及地區即時商情，亦可以在『商情快蒐』單元，以產業、地區、(商情)類別或關鍵字搜尋商情。</li>
+						</ol>
+
+						<div class="online_name">3.	經貿透視雙周刊資訊網</div>
+						<div class="onlineend_p">
+							<div class="link"><a href="https://www.trademag.org.tw/" target="_blank"><img src="images/link.svg" alt=""></a></div>
+							<div class="link_font"><a href="https://www.trademag.org.tw/" target="_blank" >(https://www.trademag.org.tw/)</a></div>
+						</div>
+
+						<ul class="number_li">
+							<li>《經貿透視》是一本專業經貿中文雜誌，透過記者深入採訪，以及由經濟部與外貿協會100 多個駐外單位，蒐集全球商情編輯而成，希望提供廠商最新的產業趨勢與市場脈動，進而獲取商機。該雜誌累積各期的地區、產業商情，形成內容豐富的資料庫，並可以關鍵字搜尋相關商情。</li>
+							<li>該資訊網並非免費服務，目前最基本的付費會員(僅限線上閱讀)費用為每年台幣1,500元。</li>
+						</ul>
+						<div class="online_name">4.	外貿協會「全球政府採購商機網」亦有『最新商情』單元</div>
+
+						<div class="onlineend_p">
+							<div class="link"><a href="https://gpa.taiwantrade.com.tw/zh/home" target="_blank"><img src="images/link.svg" alt=""></a></div>
+							<div class="link_font"><a href="https://gpa.taiwantrade.com.tw/zh/home" target="_blank" >(https://gpa.taiwantrade.com.tw/zh/home)</a></div>
+						</div>
+
+						<div class="online_name">5.	國際行銷諮詢加值型服務 IMAC Plus </div>
+
+						<ul class="number_li">
+							<li>由外貿協會「國際行銷諮詢中心」顧問，提供進階諮詢服務。</li>
+							<li>從釐清廠商需求、分析產品適銷性，剖析廠商拓銷障礙、問題點後，再提供適切拓銷策略報告，內容包含：</li>
+							<ol class="numberpoint_ul">
+							  <li>市場資訊</li>
+							  <li>主要競爭對手</li>
+							  <li>適銷通路訪談</li>
+							  <li>關稅及非關稅障礙</li>
+							  <li>產品認證及當地法規</li>
+							</ol>
+							<li>對本項服務有興趣者，歡迎至貿協「國際行銷諮詢中心」官網報名</li>
+							<ol class="numberpoint_ul">
+							  <li>承辦單位：國際行銷諮詢中心/諮詢服務組</li>
+							  <li>電話：02-27255200，分機1223 & 1225</li>
+							  <li>Email: exportadv@taitra.org.tw</li>
+							</ol>
+						</ul>
+
+					</div>
+
+				</div>
+
+
+
+				<div class="online_name">相關連結</div>
+				<div class="link_other">
+					<ul>
+						<li class="link_w_pic"><img src="images/T-00263593-L.jpg" alt=""></li>
+						<li class="link_w_pic"><img src="images/T-00263593-L.jpg" alt=""></li>
+						<li class="link_w_pic"><img src="images/T-00263593-L.jpg" alt=""></li>
+					</ul>
+				</div>
+				<div class="qekc_font">
+					<div class="yes_btn clean">
+						<div class="uij">上述資訊是否有幫助?</div>
+						<div class="kekeke adke_active">是</div>
+						<div class="kekeke">否</div>
+					</div>
+				</div>
+				<ul class="prev_btn onlinepage">
+					<a href="#">
+						<li>< Prev</li>
+					</a>
+					<!-- <a href="#"><li class="actvie">1</li></a>
+						<a href="#"><li>2</li></a>
+						<a href="#"><li>3</li></a>
+						<a href="#"><li>4</li></a>
+						<a href="#"><li>Next ></li></a> -->
+				</ul>
+			</div>
+		</section>
+    <?php require_once('i_bottom.php'); ?>
+	</body>
+</html>
