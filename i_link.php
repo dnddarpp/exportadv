579d9d289aeda2c757c7f41ed0184ff2b1e698ac
jhengivy <yozuca1123@gmail.com>
2021-02-22 10:38:11 +0800

    m

--- i_link.php
+++ i_link.php
@@ -3,11 +3,13 @@
       <div class="container">
          <ul class="linkitem">
             <!-- <li><img src="images/left.svg" alt=""></li> -->
-            <li class="olient_logo"><a href="https://www.moea.gov.tw/" target="_blank"><img src="images/T-00263593-L.jpg" alt=""></a></li>
-            <li class="olient_logo"><a href="https://www.moeaidb.gov.tw/" target="_blank"><img src="images/T-37658315.jpg" alt=""></a></li>
-            <li class="olient_logo"><a href="https://www.moeasmea.gov.tw" target="_blank"><img src="images/T-03233474.jpg" alt=""></a></li>
-            <li class="olient_logo"><a href="https://www.tipo.gov.tw" target="_blank"><img src="images/T-09297820.jpg" alt=""></a></li>
-            <li class="olient_logo"><a href="https://data.gov.tw/dataset/20394" target="_blank"><img src="images/T-38128916.jpg" alt=""></a></li>
+            <li class="olient_logo"><a href="https://www.moea.gov.tw/" target="_blank"><img src="images/T-00263593-L.png" alt="中華民國經濟部"></a></li>
+            <li class="olient_logo"><a href="https://www.trade.gov.tw/" target="_blank"><img src="images/T-86483384.png" alt="經濟部國際貿易局"></a></li>
+            <li class="olient_logo"><a href="https://www.moeaidb.gov.tw/" target="_blank"><img src="images/T-37658315.svg" alt="經濟部工業局"></a></li>
+            <li class="olient_logo"><a href="https://findbiz.nat.gov.tw/" target="_blank"><img src="images/T-75666626.png" alt="經濟部商業司"></a></li>
+            <li class="olient_logo"><a href="https://www.moeasmea.gov.tw" target="_blank"><img src="images/T-03233474.png" alt="經濟部中小企業處"></a></li>
+            <li class="olient_logo"><a href="https://www.tipo.gov.tw" target="_blank"><img src="images/T-09297820.png" alt="經濟部智慧財產局"></a></li>
+            <li class="olient_logo"><a href="https://data.gov.tw/dataset/20394" target="_blank"><img src="images/logo.2d5219c.svg" alt="我國金融機構海外分布"></a></li>
             <!-- <li><img src="images/right.svg" alt=""></li> -->
          </ul>
       </div>
