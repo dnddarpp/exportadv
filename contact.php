e73153ddfdab76542bb85975ba03ed5cb0884413
dnddar_hh <dnddar@gmail.com>
2021-03-26 04:32:35 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -25,91 +25,82 @@
 		</section>
 		<section>
 			<div class="container">
-				<div class="info_title">聯絡我們</div>
-				<div class="line"></div>
-
-
-		<div class="row ab_bottom">
-			<div class="m_bm col-12 col-md-12 col-lg-12">
-
-				<div class="contact_line"></div>
-
-				<div class="row">
-					<div class="col-12 col-md-6 col-lg-6">
-						<div class="name">
-							<input name="" type="text" placeholder="*姓名" />
-						</div>
-
-
-					</div>
-					<div class="col-12 col-md-6 col-lg-6">
-						<div class="name">
-							<input name="" type="text" placeholder="*電話" />
-						</div>
-
-					</div>
-					<div class="col-12 col-md-6 col-lg-6">
-						<div class="name">
-							<input name="" type="text" placeholder="*Eail" />
-						</div>
-
-					</div>
-					<div class="col-12 col-md-6 col-lg-6">
-						<div class="name">
-							<input name="" type="text" placeholder="*地址" />
-						</div>
-					</div>
-					<div class="col-12 col-md-6 col-lg-6">
-						<div class="name">
-							<input name="" type="text" placeholder="*主旨" />
-						</div>
-					</div>
-					<div class="col-12 col-md-6 col-lg-6">
-						<div class="name">
-							<select name="">
-								<option>詢問項目 *</option>
-								<option>發掘\商機</option>
-								<option>海外推廣活動補助</option>
-								<option>提供市場商情</option>
-								<option>海外通路建置</option>
-								<option>品牌推廣</option>
-								<option>海框運與物流</option>
-								<option>外銷人才培訓</option>
-								<option>出口融資與保險</option>
-							</select>
-						</div>
-					</div>
-					<div class="col-12 col-md-12 col-lg-12">
-						<div class="name">
-							<textarea name="" cols="" rows="" placeholder="*詢問內容" /></textarea>
-						</div>
-					</div>
-
-				</div>
-
-				<!-- <div class="chekvke">
-					<ul>
-						<li>
+			<div class="info_title">聯絡我們</div>
+			<div class="line"></div>
+			<div class="row ab_bottom">
+				<div class="m_bm col-12 col-md-12 col-lg-12">
+					<div class="contact_line"></div>
+					<div class="row">
+						<div class="col-12 col-md-6 col-lg-6">
 							<div class="name">
-								<input name="" type="text" placeholder="驗證碼" />
+								<input name="" type="text" placeholder="*姓名" />
 							</div>
-						</li>
-						<li>
-							<img src="images/cheng.svg" alt="">
-						</li>
-						<li>
-							<img src="images/number.png" alt="">
-						</li>
-					</ul>
-				</div> -->
-				<div class="check_images">
-					<img src="images/googlede.jpg" alt="">
+						</div>
+						<div class="col-12 col-md-6 col-lg-6">
+							<div class="name">
+								<input name="" type="text" placeholder="*電話" />
+							</div>
+						</div>
+						<div class="col-12 col-md-6 col-lg-6">
+							<div class="name">
+								<input name="" type="text" placeholder="*Eail" />
+							</div>
+						</div>
+						<div class="col-12 col-md-6 col-lg-6">
+							<div class="name">
+								<input name="" type="text" placeholder="*地址" />
+							</div>
+						</div>
+						<div class="col-12 col-md-6 col-lg-6">
+							<div class="name">
+								<input name="" type="text" placeholder="*主旨" />
+							</div>
+						</div>
+						<div class="col-12 col-md-6 col-lg-6">
+							<div class="name">
+								<select name="">
+									<option>詢問項目 *</option>
+									<option>發掘\商機</option>
+									<option>海外推廣活動補助</option>
+									<option>提供市場商情</option>
+									<option>海外通路建置</option>
+									<option>品牌推廣</option>
+									<option>海框運與物流</option>
+									<option>外銷人才培訓</option>
+									<option>出口融資與保險</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-12 col-md-12 col-lg-12">
+							<div class="name">
+								<textarea name="" cols="" rows="" placeholder="*詢問內容" /></textarea>
+							</div>
+						</div>
+					</div>
+					<!-- <div class="chekvke">
+						<ul>
+							<li>
+								<div class="name">
+									<input name="" type="text" placeholder="驗證碼" />
+								</div>
+							</li>
+							<li>
+								<img src="images/cheng.svg" alt="">
+							</li>
+							<li>
+								<img src="images/number.png" alt="">
+							</li>
+						</ul>
+						</div> -->
+					<div class="check_images">
+						<img src="images/googlede.jpg" alt="">
+					</div>
+					<a href="#" target="_blank">
+						<div class="goto_btn">確定送出</div>
+					</a>
 				</div>
-				<a href="#" target="_blank"><div class="goto_btn">確定送出</div></a>
-</div>
-
 			</div>
 		</section>
-    <?php require_once('i_bottom.php'); ?>
+		<?php require_once('i_bottom.php'); ?>
 	</body>
 </html>
